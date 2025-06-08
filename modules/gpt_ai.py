1f31b3c9c065b4fb7dc3aacb43c4ec5dc1ec1ba6
saiganesh74 <ganeshrejeti1@gmail.com>
2025-06-08 10:46:13 +0530

    Processing is added

---
+++ modules/gpt_ai.py
@@ -0,0 +1,17 @@
+import openai
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+def generate_storyboard(scene_description):
+    prompt = f"""
+You are a storyboard artist. Convert the following scene into 6 short storyboard frames:
+Scene: {scene_description}
+Each frame should be 1-2 sentences only. """
+    response = openai.Chatcompletion.create(
+         model="gpt-4", 
+        messages=[{"role": "user", "content": prompt}],
+        max_tokens=400,
+        temperature=0.7
+    )
+    return response['choices'][0]['message']['content']
